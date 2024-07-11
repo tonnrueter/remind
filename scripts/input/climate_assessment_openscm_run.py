5ac1002cd150fa51d9a1afaa95070abbec24fc0c
Tonn Rüter <tonnru@pik-potsdam.de>
2024-07-11 15:49:02 +0200

    Formatting

--- scripts/input/climate_assessment_openscm_run.py
+++ scripts/input/climate_assessment_openscm_run.py
@@ -1,85 +1,19 @@
-#%%
+# %%
 import argparse
 import datetime as dt
 import logging
 import os
-
-import scmdata
-# import pymagicc
 import openscm_runner
 import scmdata
-# import climate_assessment
-# from climate_assessment.climate.wg3 import clean_wg3_scenarios
 import pyam
 import pandas as pd
 import xarray as xr
-# import copy
 import json
+
 
 class EnvironmentError(Exception):
     pass
 
-# Check required environment variables. Fail if they are not set
-for env_var in ["MAGICC_EXECUTABLE_7", "MAGICC_WORKER_ROOT_DIR"]:
-    if os.environ.get(env_var, '') == '':
-        # If clause covers both cases in which the env var is not set at all 
-        # as well as the case in which it is set to an empty string
-        raise EnvironmentError(f"{env_var} does not exist")
-    # Optional debug print
-    print(f"Found '{env_var}' = '{os.environ.get(env_var)}' ")
-
-# %%
-# Parsing arguments
-parser = argparse.ArgumentParser(
-    description='Runs MAGICC for given a harmonized and infilled emissions file created by climate-assessment.'
-    )
-parser.add_argument('scens_file', type=str, 
-                    help='an integer for the accumulator')
-parser.add_argument('--climatetempdir', default=os.getcwd(),
-                    help='Temporary folder for climate assessment files')
-parser.add_argument('--endyear', default=2100,
-                    help='Final year for MAGICC runs')
-parser.add_argument('--scenario-batch-size', default=1,
-                    help='How many scenarios to run in a batch, only makes sense when running more than one')
-parser.add_argument('--probabilistic-file', required=True,
-                    help='Required, JSON file with the MAGICC parameter sets. Number of sets must match --num-cfgs')
-parser.add_argument('--num-cfgs', default=1,
-                    help='Number of parameter sets must match --probabilistic-file')
-
-
-args = parser.parse_args()
-print(args)
-
-
-
-
-# %%
-# Setting parameters
-# REMOVE 
-# args = argparse.Namespace(scens_file='/p/projects/piam/abrahao/scratch/remind_tonn/output/N_SSP2EU-NPi-internalize-ar6_2024-06-12_10.39.45/climate-assessment-data-tirf/allpulses.xlsx', climatetempdir='/p/projects/piam/abrahao/scratch/remind_tonn/output/N_SSP2EU-NPi-internalize-ar6_2024-06-12_10.39.45/climate-assessment-data-tirf', endyear='2205', scenario_batch_size='1', probabilistic_file='/p/projects/piam/abrahao/scratch/remind_tonn/output/N_SSP2EU-NPi-internalize-ar6_2024-06-12_10.39.45/probmod.json', num_cfgs='1')
-# END REMOVE 
-
-# Harmonized and infilled emissions scenario
-basescenfname = args.scens_file
-
-# JSON with parameter sets
-climate_assessment_magicc_prob_file_iteration = args.probabilistic_file
-
-# Final year for MAGICC run
-endyear = args.endyear
-
-# Output file name, assumed to be inside climatetempdir
-# TODO: We don't really use climatetempdir here, 
-# so we might want to replace it with a --outfilename 
-# argument, with default in the current working directory
-outfilename = os.path.join(args.climatetempdir,"openscm_output.xlsx")
-
-# %% 
-# Check arguments
-
-# %%
-# Clean the harmonized and infilled scenario, fixing some variable names required
-inbasescen = pyam.IamDataFrame(basescenfname)
 
 # %%
 def clean_wg3_scenarios(inp):
@@ -155,44 +89,108 @@ def clean_wg3_scenarios(inp):
         pass
 
     return clean_scenarios
-#%%
+
+
+# Check required environment variables. Fail if they are not set
+for env_var in ["MAGICC_EXECUTABLE_7", "MAGICC_WORKER_ROOT_DIR"]:
+    if os.environ.get(env_var, "") == "":
+        # If clause covers both cases in which the env var is not set at all
+        # as well as the case in which it is set to an empty string
+        raise EnvironmentError(f"{env_var} does not exist")
+    # Optional debug print
+    print(f"Found '{env_var}' = '{os.environ.get(env_var)}' ")
+
+# %%
+# Parsing arguments
+parser = argparse.ArgumentParser(
+    description="Runs MAGICC for given a harmonized and infilled emissions file created by climate-assessment."
+)
+parser.add_argument("scens_file", type=str, help="an integer for the accumulator")
+parser.add_argument(
+    "--climatetempdir",
+    default=os.getcwd(),
+    help="Temporary folder for climate assessment files",
+)
+parser.add_argument("--endyear", default=2100, help="Final year for MAGICC runs")
+parser.add_argument(
+    "--scenario-batch-size",
+    default=1,
+    help="How many scenarios to run in a batch, only makes sense when running more than one",
+)
+parser.add_argument(
+    "--probabilistic-file",
+    required=True,
+    help="Required, JSON file with the MAGICC parameter sets. Number of sets must match --num-cfgs",
+)
+parser.add_argument(
+    "--num-cfgs",
+    default=1,
+    help="Number of parameter sets must match --probabilistic-file",
+)
+
+
+args = parser.parse_args()
+print(args)
+
+
+# %%
+# Setting parameters
+# REMOVE
+# args = argparse.Namespace(scens_file='/p/projects/piam/abrahao/scratch/remind_tonn/output/N_SSP2EU-NPi-internalize-ar6_2024-06-12_10.39.45/climate-assessment-data-tirf/allpulses.xlsx', climatetempdir='/p/projects/piam/abrahao/scratch/remind_tonn/output/N_SSP2EU-NPi-internalize-ar6_2024-06-12_10.39.45/climate-assessment-data-tirf', endyear='2205', scenario_batch_size='1', probabilistic_file='/p/projects/piam/abrahao/scratch/remind_tonn/output/N_SSP2EU-NPi-internalize-ar6_2024-06-12_10.39.45/probmod.json', num_cfgs='1')
+# END REMOVE
+
+# Harmonized and infilled emissions scenario
+basescenfname = args.scens_file
+
+# JSON with parameter sets
+climate_assessment_magicc_prob_file_iteration = args.probabilistic_file
+
+# Final year for MAGICC run
+endyear = args.endyear
+
+# Output file name, assumed to be inside climatetempdir
+# TODO: We don't really use climatetempdir here,
+# so we might want to replace it with a --outfilename
+# argument, with default in the current working directory
+outfilename = os.path.join(args.climatetempdir, "openscm_output.xlsx")
+
+# %%
+# Check arguments
+
+# %%
+# Clean the harmonized and infilled scenario, fixing some variable names required
+inbasescen = pyam.IamDataFrame(basescenfname)
+
+# %%
 basescen = clean_wg3_scenarios(inbasescen)
-#%%
+# %%
 # Read parameter sets, can also be multiple
 with open(climate_assessment_magicc_prob_file_iteration) as f:
     allparsets = json.load(f)
 allparsets = [i["nml_allcfgs"] for i in allparsets["configurations"]]
 
-# Set endyear in all parsets 
+# Set endyear in all parsets
 for parset in allparsets:
     parset["endyear"] = int(endyear)
 
 
 # %%
 runresults = openscm_runner.run(
-    climate_models_cfgs={
-        "MAGICC7" : allparsets
-    },
+    climate_models_cfgs={"MAGICC7": allparsets},
     output_variables=(
         "Surface Air Temperature Change",
-        "Net Atmosphere to Land Flux|CO2"
+        "Net Atmosphere to Land Flux|CO2",
     ),
-scenarios = scmdata.ScmRun(basescen)
-    )
+    scenarios=scmdata.ScmRun(basescen),
+)
 
 # %%
 # Write output
-
-outfilename = os.path.splitext(basescenfname)[0] + '_IAMC_climateassessment.xlsx'
-
-runresults.filter(region = "World"
-                  ).to_iamdataframe(
-                      
-                  ).swap_time_for_year(
-                      
-                  ).to_excel(
-                      outfilename
-                      )
+outfilename = os.path.splitext(basescenfname)[0] + "_IAMC_climateassessment.xlsx"
+runresults.filter(region="World")\
+          .to_iamdataframe()\
+          .swap_time_for_year()\
+          .to_excel(outfilename)
 
 # # # %%
 # # # Show basic results
